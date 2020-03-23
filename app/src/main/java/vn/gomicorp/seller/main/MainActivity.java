40d4d7118cceeaf7ef1833d16003e40e26ac5693
Khoi Le <khoiln218@gmail.com>
2020-03-23 11:38:28 +0700

    -[feat](mainpage) add layout

--- app/src/main/java/vn/gomicorp/seller/main/MainActivity.java
+++ app/src/main/java/vn/gomicorp/seller/main/MainActivity.java
@@ -1,16 +1,84 @@
 package vn.gomicorp.seller.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import vn.gomicorp.seller.R;
+import vn.gomicorp.seller.main.home.HomeFragment;
+import vn.gomicorp.seller.main.market.MarketFragment;
+import vn.gomicorp.seller.main.mypage.MyPageFragment;
+import vn.gomicorp.seller.main.notification.NotificationFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigation_main);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment);
+        if (fragment != null)
+            fragment.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                loadFragment(new HomeFragment());
+                return true;
+
+            case R.id.nav_market:
+                loadFragment(new MarketFragment());
+                return true;
+
+            case R.id.nav_notify:
+                loadFragment(new NotificationFragment());
+                return true;
+
+            case R.id.nav_account:
+                loadFragment(new MyPageFragment());
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
     }
 }
