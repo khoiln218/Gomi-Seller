aed7f0e3f4f5e19be8b83224db2c22cf1149b3b1
Khoi Le <khoiln218@gmail.com>
2020-04-22 11:47:59 +0700

    [api] set local EndPoint

--- app/src/main/java/vn/gomicorp/seller/data/source/remote/EndPoint.java
+++ app/src/main/java/vn/gomicorp/seller/data/source/remote/EndPoint.java
@@ -1,5 +1,5 @@
 package vn.gomicorp.seller.data.source.remote;
 
 public interface EndPoint {
-    String BASE_URL = "http://tipstore.vn/api/";
+    String BASE_URL = "http://192.168.1.33:2525/api/";
 }
